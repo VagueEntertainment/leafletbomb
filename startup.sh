5fb80e18d0c1a7244a70d57d1eea1466ebca24e1
Benjamin Flanagin <bflanagin@vagueentertainment.com>
2017-10-11 15:01:00 -0500

    startup script and settings.conf-sample update

--- startup.sh
+++ startup.sh
@@ -8,8 +8,15 @@ S=(`gawk  /server=/{print} settings.conf |  tr "=" " "`)
 #echo ${S[1]}
 p=(`gawk  /port=/{print} settings.conf |  tr "=" " "`)
 #echo ${p[1]}
+sec=(`gawk  /secure=/{print} settings.conf |  tr "=" " "`)
 
-export MAIL_URL='smtp://'${U[1]}':'${P[1]}'@'${S[1]}':'${p[1]}
+if [ "${sec[1]}" == "true" ]; then 
+    serv='smtps'
+   else 
+    serv='smtp'
+    fi
+
+export MAIL_URL=$serv'://'${U[1]}':'${P[1]}'@'${S[1]}':'${p[1]}
 
 echo $MAIL_URL
 
